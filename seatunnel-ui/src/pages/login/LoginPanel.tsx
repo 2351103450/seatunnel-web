6532969b9271c6fbd34ac5a47cd0c7f627ad2613
upcoder <weifuwan007@163.com>
2026-03-06 10:11:32 +0800

    update overflow attr

--- seatunnel-ui/src/pages/login/LoginPanel.tsx
+++ seatunnel-ui/src/pages/login/LoginPanel.tsx
@@ -233,7 +233,7 @@ export default function LoginPanel({ onFire }: LoginPanelProps) {
         width: "100%",
         height: "100vh",
         position: "relative",
-        overflowY: "auto",
+        overflow: "hidden",
       }}
     >
       <style>{keyframes}</style>
