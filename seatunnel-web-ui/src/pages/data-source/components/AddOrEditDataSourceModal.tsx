ba2838209b1f7a5566a591d6bba0c19adf9aed2c
weifuwan <295227940@qq.com>
2026-06-29 09:48:36 +0800

    feat: improve savepoint

--- seatunnel-web-ui/src/pages/data-source/components/AddOrEditDataSourceModal.tsx
+++ seatunnel-web-ui/src/pages/data-source/components/AddOrEditDataSourceModal.tsx
@@ -244,7 +244,7 @@ const AddOrEditDataSourceModal = forwardRef<DataSourceModalRef>((_, ref) => {
         body: {
           padding: "20px 24px 16px",
           background: "#F8FAFC",
-          maxHeight: "82vh",
+          maxHeight: "69vh",
           overflowY: "auto",
           minHeight: "65vh",
         },
